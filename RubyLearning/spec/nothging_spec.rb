e71bd84229f8b2c87aafc737c30a5cd94f47c6d5
ma_kang <yuzhoun@163.com>
2011-09-27 20:55:38 +0800

    add rspec file

---
+++ RubyLearning/spec/nothging_spec.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'nothging'
+
+describe Nothging do
+  before(:each) do
+    @nothging = Nothging.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
+
