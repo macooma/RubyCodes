a52b158e181e753bf3dac124f0d0f2ae9a305003
ma_kang <yuzhoun@163.com>
2011-11-10 12:45:53 +0800

    modify

--- RubyLearning/lib/main.rb
+++ RubyLearning/lib/main.rb
@@ -12,11 +12,15 @@ class Song
     #     @duration = _duration
     # end
     #
-
+    include Comparable
     def initialize(name, artist, duration)
         @name = name
         @artist = artist
         @duration = duration
+    end
+    
+    def <=> (other) 
+      self.duration <=> other.duration
     end
     
     def to_s
@@ -84,38 +88,38 @@ end
 
 # require 'test/unit'
 # class TestSongList < Test::Unit::TestCase
-    # def test_delete
-        # list = SongList.new
-        # s1 = Song.new('title1', 'artist1', 1)
-        # s2 = Song.new('title2', 'artist2', 2)
-        # s3 = Song.new('title3', 'artist3', 3)
-        # s4 = Song.new('title4', 'artist4', 4)
-        # list.append(s1).append(s2).append(s3).append(s4)
-        # assert_equal(s1, list[0])
-        # assert_equal(s3, list[2])
-        # assert_nil(list[9])
-        # assert_equal(s1, list.delete_first)
-        # assert_equal(s2, list.delete_first)
-        # assert_equal(s4, list.delete_last)
-        # assert_equal(s3, list.delete_last)
-        # assert_nil(list.delete_last)
-    # end
+#     def test_delete
+#         list = SongList.new
+#         s1 = Song.new('title1', 'artist1', 1)
+#         s2 = Song.new('title2', 'artist2', 2)
+#         s3 = Song.new('title3', 'artist3', 3)
+#         s4 = Song.new('title4', 'artist4', 4)
+#         list.append(s1).append(s2).append(s3).append(s4)
+#         assert_equal(s1, list[0])
+#         assert_equal(s3, list[2])
+#         assert_nil(list[9])
+#         assert_equal(s1, list.delete_first)
+#         assert_equal(s2, list.delete_first)
+#         assert_equal(s4, list.delete_last)
+#         assert_equal(s3, list.delete_last)
+#         assert_nil(list.delete_last)
+#     end
 # end
 
-File.open("./songdata") do |song_file|
-  songs = SongList.new
-  song_file.each do |line|
-    file, length, name, title = line.chomp.split(/\s*\|\s*/)
-    name.squeeze!(" ")
-    mins, secs = length.split(/:/)
-    # the same as above: mins, secs = length.scan(/\d+/)
-    song = Song.new(title, name, mins.to_i*60 + secs.to_i)
-    songs.append(song)
-    puts song 
-  end
-  p songs
-  puts "Search result:"
-  puts songs.lookup("ain't")
-  puts songs.lookup("RED")
-  puts songs.lookup("WoRlD")
-end
+#File.open("./songdata") do |song_file|
+#  songs = SongList.new
+#  song_file.each do |line|
+#    file, length, name, title = line.chomp.split(/\s*\|\s*/)
+#    name.squeeze!(" ")
+#    mins, secs = length.split(/:/)
+#    # the same as above: mins, secs = length.scan(/\d+/)
+#    song = Song.new(title, name, mins.to_i*60 + secs.to_i)
+#    songs.append(song)
+#    puts song 
+#  end
+#  p songs
+#  puts "Search result:"
+#  puts songs.lookup("ain't")
+#  puts songs.lookup("RED")
+#  puts songs.lookup("WoRlD")
+#end
--- RubyLearning/test/class_test.rb
+++ RubyLearning/test/class_test.rb
@@ -4,12 +4,16 @@
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
 require 'test/unit'
-require 'class'
+require 'main'
 
-class ClassTest < Test::Unit::TestCase
+class MainTest < Test::Unit::TestCase
   def test_foo
-    #TODO: Write test
-    flunk "TODO: Write test"
-    # assert_equal("foo", bar)
+    song1 = Song.new("My Way", "Sinatra", 225)
+    song2 = Song.new("Bicylops", "Fleck", 260)
+
+    assert_equal(-1, song1 <=> song2)
+    assert_equal(true, song1 < song2)
+    assert_equal(false, song1 == song1)
+    assert_equal(false, song1 > song2)
   end
 end
